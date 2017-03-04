965c3afed60c6cb0e3662be93b16346d9dfdb235
alicja-cyganiewicz <alicja.cyganiewicz@infakt.pl>
2017-03-04 13:08:29 +0000

    Basic user devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170226215553) do
+ActiveRecord::Schema.define(version: 20170304130633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "abilities", force: :cascade do |t|
-    t.string "name"
-  end
-
-  create_table "abilities_users", id: false, force: :cascade do |t|
-    t.integer "ability_id", null: false
-    t.integer "user_id",    null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -38,11 +29,9 @@ ActiveRecord::Schema.define(version: 20170226215553) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
