f89540d4cff48cecfcc0ae11487bfb8d542170ec
alicja-cyganiewicz <alicja.cyganiewicz@infakt.pl>
2017-03-04 20:31:14 +0000

    Undefined

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-    address: smtp.emaillabs.net.pl,
+    address: 'smtp.emaillabs.net.pl',
     port: 587,
     user_name: ENV['email_username'],
     password: ENV['email_password'],
