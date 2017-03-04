12060d73f87562f5c99e708fad234f2a3895708c
alicja-cyganiewicz <alicja.cyganiewicz@infakt.pl>
2017-03-04 16:55:41 +0000

    API point: without discarded IDS

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,6 +2,9 @@ module Api
   class UsersController < ::ApplicationController
     def with_ability
       @users = Ability.where('lower(name) = ?', params['ability_name'].downcase).first.try(:users)
+      if params["discarded_ids"].present?
+        @users = @users.where.not(id: params["discarded_ids"].values)
+      end
     end
   end
 end
