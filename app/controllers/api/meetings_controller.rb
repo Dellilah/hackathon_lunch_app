916e90a1b8b8d647aa3406c25cfdd1aef885da48
alicja-cyganiewicz <alicja.cyganiewicz@infakt.pl>
2017-03-05 00:51:13 +0000

    Sending SMS on confirming/rejecting an invitation

--- app/controllers/api/meetings_controller.rb
+++ app/controllers/api/meetings_controller.rb
@@ -1,3 +1,8 @@
+require 'net/http'
+require 'uri'
+require 'json'
+require 'mime/types'
+
 module Api
   class MeetingsController < ::ApplicationController
     skip_before_action :verify_authenticity_token
@@ -19,6 +24,7 @@ module Api
       @meeting = Meeting.find(params[:meeting_id])
       @meeting.confirm
       MeetingsMailer.invitation_confirmed(@meeting.invitor, @meeting).deliver_later
+      send_sms(@meeting.invitor, @meeting)
       render json: {}, status: :ok
     end
 
@@ -26,7 +32,25 @@ module Api
       @meeting = Meeting.find(params[:meeting_id])
       @meeting.reject
       MeetingsMailer.invitation_rejected(@meeting.invitor, @meeting).deliver_later
+      send_sms(@meeting.invitor, @meeting)
       render json: {}, status: :ok
+    end
+
+    def send_sms(user, meeting)
+      uri = URI.parse("https://rest.nexmo.com/sms/json")
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+
+      request = Net::HTTP::Post.new(uri.path, {'Content-Type' => 'application/json'})
+      request.body = {
+        api_key: ENV['nexmo_api_key'],
+        api_secret: ENV['nexmo_api_secret'],
+        to: user.phone_number.gsub(/\+|\-|\s/, ""),
+        from: 'Lunch App',
+        text: "Your invitation to lunch with #{meeting.invited.first_name} has been #{meeting.status}."
+      }.to_json
+
+      response = http.request(request)
     end
   end
 end
