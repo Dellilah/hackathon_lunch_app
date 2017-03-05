18f06739031252c0c944134dc28c46df3a143cf9
alicja-cyganiewicz <alicja.cyganiewicz@infakt.pl>
2017-03-05 04:18:42 +0000

    testing

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -13,14 +13,17 @@ module Api
     def with_ability_and_datetime
       current_user = User.find(params['current_user_id'])
       nearby_users =  current_user.nearbys(3).map(&:id)
+      puts "*"*100
+      puts params['time'].gsub(":00", "").to_i
+      puts "*"*100
       @users = Ability.where('lower(name) = ?', params['ability_name'].downcase).first.try(:users)
       if @users
         @users = @users.where(id: nearby_users)
         @users = @users.select { |user|
           user.days &&
           user.days.split(',').include?(params['day_number']) &&
-          user.from_hour.to_i <= params['time'].gsub(":00", "") &&
-          user.to_hour.to_i >= params['time'].gsub(":00", "")
+          user.from_hour.to_i <= params['time'].gsub(":00", "").to_i &&
+          user.to_hour.to_i >= params['time'].gsub(":00", "").to_i
         }
       end
       if params["discarded_ids"].present?
